b73aec5acba64f6800c56e718d6c30379ea722f8
Ara Ovsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2024-02-17 01:23:02 +0300

    Update Main.jsx

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,8 +1,32 @@
 import React from "react";
+import laptop from '../assets/svg/laptop.png'
+import '../css/main.css'
 
 const Main = (() => {
   return (
-    <h1>Main</h1>
+    <main>
+      <section className="wrapper">
+        <div className="container">
+          <div className="grid-row-2">
+            <div className="grid-item-1">
+              <h1 className="main-heading">
+              ТВОЙ ИДЕАЛЬНЫЙ
+              <br />
+              ИНСТРУМЕНТ ДЛЯ
+              <br />
+              ОНЛАЙН-ЗАНЯТИЙ
+              </h1>
+              <p className="info-text">
+              Bсе нужные инструменты в одном месте
+              </p>
+            </div>
+            <div className="grid-item-2">
+              <img className="laptop"src={laptop} alt=""/>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
   )
 })
 
