2f1fd2e95871e413f7a0c2338b479a680e9865b7
Ara Ovsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2024-02-17 01:33:59 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
-import './css/root.css'
+import { ParallaxProvider } from 'react-scroll-parallax';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ParallaxProvider>
+        <App />
+    </ParallaxProvider>
 );
