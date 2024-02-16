90727ca2aeac5c8829a67fb21e63771d114e7bfd
Ara Ovsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2024-02-17 01:42:43 +0300

    Summary

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import Header from './Components/Header';
-import NavBar from './Components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
       <Header/>
     </div>
   );
