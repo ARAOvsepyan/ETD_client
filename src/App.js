a882ad0dce6b536e6643c8c2a4b577eee31bb4b3
Ara Ovsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2024-02-17 22:50:13 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import Header from './Components/Header';
 import Nav from './Components/Nav';
+import AbilityList from './Components/AbilityList';
 
 function App() {
   return (
     <div className="App">
       <Nav/>
       <Header/>
+      <AbilityList/>
+      <AbilityList/>
+      <AbilityList/>
+
     </div>
   );
 }
