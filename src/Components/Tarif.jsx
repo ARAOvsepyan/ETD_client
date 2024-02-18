0eeda35249219fcafc9ae5cfd2ed87fbbecacf55
Ara Ovsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2024-02-18 23:57:58 +0300

    Create Tarif.jsx

---
+++ src/Components/Tarif.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Parallax } from 'react-scroll-parallax'
+
+const Tarif = () => {
+  return (
+    <div className="tarifs">
+      <div className="hover-text-one">
+        <figure className="effect-text-two">
+           <div className='photo beginner'/>
+            <figcaption>
+	            <h1>БАЗОВЫЙ</h1>
+	            <p>До 5 учнеников <br/> <br/>
+                  Доступ ко всем инструментам <br/> <br/>
+                  Хранение файлов до 750 МБ
+              </p>
+            </figcaption>			
+        </figure>
+      </div>
+      <div className="hover-text-one">
+        <figure className="effect-text-two">
+           <div className='photo advance'/>
+            <figcaption>
+	            <h1>ПРОДВИНУТЫЙ</h1>
+	            <p>До 5 учнеников <br/> <br/>
+                  Доступ ко всем инструментам <br/> <br/>
+                  Хранение файлов до 750 МБ
+              </p>
+            </figcaption>			
+        </figure>
+      </div>
+      <div className="hover-text-one">
+        <figure className="effect-text-two">
+           <div className='photo premium'/>
+            <figcaption>
+	            <h1>ПРЕМИУМ</h1>
+	            <p>До 5 учнеников <br/> <br/>
+                  Доступ ко всем инструментам <br/> <br/>
+                  Хранение файлов до 750 МБ
+              </p>
+            </figcaption>			
+        </figure>
+      </div>
+    </div>
+  )
+}
+
+export default Tarif
