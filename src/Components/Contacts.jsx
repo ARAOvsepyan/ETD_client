6d1de35080b4fd514276d1cf2d19cf4720cb964d
Ara Ovsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2024-02-18 23:57:56 +0300

    Create Contacts.jsx

---
+++ src/Components/Contacts.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import laptop from '../images/laptop.png'
+
+function Contacts() {
+  return (
+    <div className='contacts'>
+      <div className='contacts_wrapper'>
+        <div className='row'>
+
+        </div>
+        <div className='row'>
+
+        </div>
+        <div className='row'>
+          
+        </div>
+        <div className='row'>
+          
+        </div>
+      </div>
+      <div className='image-wrapper contacts_img'>
+        <img src={laptop} alt="" className='image-item'/>
+      </div>
+    </div>
+  )
+}
+
+export default Contacts
