4415e296ba9a796fe721131aac466e0066ab3361
Ara Ovsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2024-02-20 02:16:54 +0300

    Update ClassName

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,26 +1,32 @@
 import React from 'react'
-import laptop from '../images/laptop.png'
+import laptop from '../assets/icons/laptop.png'
 import { Parallax } from "react-scroll-parallax";
+import { Link } from 'react-scroll'
+
 
 const Header = () => {
 
   return (
-    <div className='main'>
+    <div className='header'>
     <Parallax speed={-10}>
-      <div className='text'>
+      <div className='header__text'>
         <h1>ТВОЙ ИДЕАЛЬНЫЙ<br/>
             ИНСТРУМЕНТ ДЛЯ<br/>
             ОНЛАЙН-ЗАНЯТИЙ
         </h1>
-        <p className='details'>Все нужные инструменты в одном месте</p>
+        <p className='header__details'>Все нужные инструменты в одном месте</p>
       </div>
       </Parallax>
-      <div className='image-wrapper'>
+      <div className='header__image-wrapper'>
         <img src={laptop} alt="" className='image-item'/>
         <div class="image-btn">
           <a href='#' className='cv-btn'>Попробовать бесплатно</a>
           <div className='scroll-text'>
-            <a href='#' className='cv-text'>Возможности</a>
+            <Link to={'ability'} 
+                smooth={true} 
+                duration={500}
+                className='cv-text'
+            >Возможности </Link>
             <div id="scroll-down">
               <span class="arrow-down"></span>
             </div>
