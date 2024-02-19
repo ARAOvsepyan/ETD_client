ad39bd0517fcbdb3385a31187f7763e814b125af
Ara Ovsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2024-02-20 02:16:41 +0300

    Add LaptopAnimation

--- src/Components/Contacts.jsx
+++ src/Components/Contacts.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
-import laptop from '../images/laptop.png'
+import React, {useEffect, useRef, useState, useMemo} from 'react'
+import monitor from '../assets/images/monitor.png'
+
+function useIsInViewport(ref) {
+  const [isIntersecting, setIsIntersecting] = useState(false);
+
+  const observer = useMemo(
+    () =>
+      new IntersectionObserver(([entry]) =>
+        setIsIntersecting(entry.isIntersecting),
+      ),
+    [],
+  );
+
+  useEffect(() => {
+    observer.observe(ref.current);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [ref, observer]);
+
+  return isIntersecting;
+}
 
 function Contacts() {
+  const ref = useRef(null);
+
+  const isInViewport1 = useIsInViewport(ref);
+
   return (
-    <div className='contacts'>
+    <div className='contacts' id='contacts'>
       <div className='contacts_wrapper'>
         <div className='row'>
 
@@ -18,9 +44,15 @@ function Contacts() {
           
         </div>
       </div>
-      <div className='image-wrapper contacts_img'>
-        <img src={laptop} alt="" className='image-item'/>
-      </div>
+      <div className={isInViewport1 ? 'laptop js-laptop laptop--closed' : 'laptop js-laptop'}>
+        <div class="laptop-top">
+          <div class="laptop__lid"></div>
+          <div class="laptop__screen">
+            <img src={monitor} alt="" className='laptop__top-img'/>
+          </div>
+        </div>
+        <div ref={ref} class="laptop__base"></div>
+        </div>
     </div>
   )
 }
